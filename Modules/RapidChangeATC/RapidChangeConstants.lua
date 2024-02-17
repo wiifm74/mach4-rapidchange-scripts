361e69545f06b772994f1671c2deec9bc5a6ce15
James Greilick <41349921+jgreilick@users.noreply.github.com>
2024-02-17 16:36:59 -0600

    settings defined

--- Modules/RapidChangeATC/RapidChangeConstants.lua
+++ Modules/RapidChangeATC/RapidChangeConstants.lua
@@ -1,4 +1,58 @@
 local RapidChangeConstants = {}
+
+-- Setting Types
+RapidChangeConstants.DISTANCE_SETTING = 0
+RapidChangeConstants.UDISTANCE_SETTING = 1
+RapidChangeConstants.FEED_SETTING = 2
+RapidChangeConstants.RPM_SETTING = 3
+RapidChangeConstants.MCODE_SETTING = 4
+RapidChangeConstants.OPTION_SETTING = 5
+RapidChangeConstants.SWITCH_SETTING = 6
+RapidChangeConstants.COUNT_SETTING = 7
+RapidChangeConstants.PORT_SETTING = 8
+RapidChangeConstants.PIN_SETTING = 9
+RapidChangeConstants.DWELL_SETTING = 10
+
+--UI Control Types
+RapidChangeConstants.INPUT_CONTROL = 0
+RapidChangeConstants.CHECK_CONTROL = 1
+RapidChangeConstants.RADIO_CONTROL = 2
+RapidChangeConstants.SELECT_CONTROL = 3
+RapidChangeConstants.LISTBOX_CONTROL = 4
+RapidChangeConstants.CHOICE_CONTROL = 5
+
+
+-- Options
+RapidChangeConstants.ALIGNMENT_OPTIONS = {
+  { value = RapidChangeConstants.X_AXIS, label = "X Axis" },
+  { value = RapidChangeConstants.Y_AXIS, label = "Y Axis" },
+}
+
+RapidChangeConstants.DIRECTION_OPTIONS = {
+  { value = RapidChangeConstants.POSITIVE, label = "Positive" },
+  { value = RapidChangeConstants.NEGATIVE, label = "Negative" },
+}
+
+RapidChangeConstants.COVER_AXIS_OPTIONS = {
+  { value = RapidChangeConstants.A_AXIS, label = "A Axis" },
+  { value = RapidChangeConstants.B_AXIS, label = "B Axis" },
+  { value = RapidChangeConstants.C_AXIS, label = "C Axis" },
+}
+
+RapidChangeConstants.COVER_CONTROL_OPTIONS = {
+  { value = RapidChangeConstants.COVER_CONTROL_AXIS, label = "Axis" },
+  { value = RapidChangeConstants.COVER_CONTROL_OUTPUT, label = "Output" },
+}
+
+RapidChangeConstants.UNIT_OPTIONS = {
+  { value = RapidChangeConstants.MILLIMETERS, label = "Millimeters" },
+  { value = RapidChangeConstants.INCHES, label = "Inches" },
+}
+
+-- Switch Setting state
+RapidChangeConstants.DISABLED = 0
+RapidChangeConstants.ENABLED = 1
+
 --Units
 RapidChangeConstants.INCHES = 20
 RapidChangeConstants.MILLIMETERS = 21
@@ -16,13 +70,7 @@ RapidChangeConstants.B_AXIS = 4
 RapidChangeConstants.C_AXIS = 5
 
 --Dust Cover
-RapidChangeConstants.COVER_CONTROL_DISABLE = 0
-RapidChangeConstants.COVER_CONTROL_AXIS = 1
-RapidChangeConstants.COVER_CONTROL_OUTPUT = 2
-
---Tool Recognition
-RapidChangeConstants.TOOL_RECOGNITION_DISABLE = 0
-RapidChangeConstants.TOOL_RECOGNITION_ENABLE = 0
-RapidChangeConstants.TOOL_RECOGNITION_OVERRIDE = 0
+RapidChangeConstants.COVER_CONTROL_AXIS = 0
+RapidChangeConstants.COVER_CONTROL_OUTPUT = 1
 
 return RapidChangeConstants
--- Modules/RapidChangeATC/RapidChangeSettings.lua
+++ Modules/RapidChangeATC/RapidChangeSettings.lua
@@ -1,98 +1,196 @@
---[[
-This file contains an array of settings for RapidChangeATC.
-
-Each setting is indexed by it's storage key and has the following properties:
-  label: Label for GUI controls
-  description: Description for tool tips.
-  default: Default value.
-  options: List of options for select controls.
-  value: The current value for the setting.
-
-There is one public function: RapidChangeSettings.SetValue(key, value)
-  This function will update the profile storage to the supplied value and then 
-  retrieve the stored value and assign it to the setting's value property.
---]]
-
 local RapidChangeSettings = {}
 
+--Get Mach4 instance
 local inst = mc.mcGetInstance()
 
---Constants
-RC_SECTION = "RapidChangeATC"
-INT_SETTING = 0
-DOUBLE_SETTING = 1
-STRING_SETTING = 2
-ALIGNMENT_OPTIONS = { rcConst.X_AXIS, rcConst.Y_AXIS }
-DIRECTION_OPTIONS = { rcConst.POSITIVE, rcConst.NEGATIVE }
-UNIT_OPTIONS = { rcConst.MILLIMETERS, rcConst.INCHES }
+--Setting definition creation helpers
+local function createDefinition(key, label, description, settingType, defaultValue, options)
+  defaultValue = defaultValue or {}
+  options = options or {}
 
-
---Local functions
-
---Storage retrieval
-local function getValue(key)
-  if RapidChangeSettings[key].storageType == DOUBLE_SETTING then
-    return mc.mcProfileGetDouble(inst, RC_SECTION, key, RapidChangeSettings[key].default)
-  elseif RapidChangeSettings[key].storageType == INT_SETTING then
-    return mc.mcProfileGetInt(inst, RC_SECTION, key, RapidChangeSettings[key].default)
-  else
-    return mc.mcProfileGetString(inst, RC_SECTION, key, RapidChangeSettings[key].default)
-  end
-end
-
--- Setting creation
-local function createSetting(key, label, description, default, options, storageType)
-  RapidChangeSettings[key] = {
+  return {
+    key = key,
     label = label,
     description = description,
-    default = default,
-    options = options,
-    storageType = storageType
+    settingType = settingType,
+    defaultValue = defaultValue,
+    options = options
   }
-
-  RapidChangeSettings[key].value = getValue(key)
 end
 
-local function createSettingDouble(key, label, description, default)
-  if default == nil then default = 0.000 end
-  createSetting(key, label, description, default, {}, DOUBLE_SETTING)
+local function createOptionDefinition(key, label, description, options)
+  return createDefinition(key, label, description, k.OPTION_SETTING, options[1], options)
 end
 
-local function createSettingInt(key, label, description, default)
-  if default == nil then default = 0 end
-  createSetting(key, label, description, default, {}, INT_SETTING)
-end
+--Setting definitions defined in display order for UI looping
+local definitions = {
+  --Tool Change
+  createOptionDefinition("Units", "Units", "Units for this configuration.", k.UNIT_OPTIONS),
+  createOptionDefinition("Alignment", "Alignment", "Axis on which the magazine is aligned.", k.ALIGNMENT_OPTIONS),
+  createOptionDefinition("Direction", "Direction", "Direction of travel from Pocket 1 to Pocket 2.", k.DIRECTION_OPTIONS),
+  createDefinition("PocketCount", "Pocket Count", "The number of tool pockets in your magazine.", k.COUNT_SETTING),
+  createDefinition("PocketOffset", "Pocket Offset", "Distance between pockets (center-to-center) along the alignment axis.", k.UDISTANCE_SETTING),
+  createDefinition("XPocket1", "X Pocket 1", "X Position (Machine Coordinates) of the center of Pocket 1.", k.DISTANCE_SETTING),
+  createDefinition("YPocket1", "Y Pocket 1", "Y Position (Machine Coordinates) of the center of Pocket 1.", k.DISTANCE_SETTING),
+  createDefinition("XManual", "X Manual", "X Position (Machine Coordinates) for manual tool changes.", k.DISTANCE_SETTING),
+  createDefinition("YManual", "Y Manual", "Y Position (Machine Coordinates) for manual tool changes.", k.DISTANCE_SETTING),
+  createDefinition("ZEngage", "Z Engage", "Z Position (Machine Coordinates) for full engagement with the clamping nut.", k.DISTANCE_SETTING),
+  createDefinition("ZMoveToLoad", "Z Move To Load", "Z Position to rise to after unloading a tool, before moving to the pocket for loading.", k.DISTANCE_SETTING),
+  createDefinition("ZMoveToProbe", "Z Move To Probe", "Z Position to rise to after loading a tool, before moving to the tool setter for probing.", k.DISTANCE_SETTING),
+  createDefinition("ZSafeClearance", "Z Safe Clearance", "Z Position for the safe clearance of all obstacles.", k.DISTANCE_SETTING),
+  createDefinition("LoadRPM", "Load RPM", "Spindle speed for loading a tool (Clockwise operation).", k.RPM_SETTING),
+  createDefinition("UnloadRPM", "Unload RPM", "Spindle speed for unloading a tool (Counterclockwise operation).", k.RPM_SETTING),
+  createDefinition("EngageFeedRate", "Engage Feed Rate", "Feed rate for the engagement process.", k.FEED_SETTING),
 
-local function createSettingOption(key, label, description, options)
-  createSetting(key, label, description, options[1], options, INT_SETTING)
-end
+  --Tool Touch Off
+  createDefinition("TouchOffEnabled", "Tool Touch Off Enabled", "Calls the configured Tool Touch Off M-Code after loading a tool.", k.SWITCH_SETTING),
+  createDefinition("TouchOffMCode", "Tool Touch Off M-Code", "The M-Code for the tool touch off macro to be called after loading a tool.", k.MCODE_SETTING, 131),
+  createDefinition("XToolSetter", "X Tool Setter", "X Position (Machine Coordinates) of the center of the tool setter.", k.DISTANCE_SETTING),
+  createDefinition("YToolSetter", "Y Tool Setter", "Y Position (Machine Coordinates) of the center of the tool setter.", k.DISTANCE_SETTING),
+  createDefinition("ZSeekStart", "Z Seek Start", "Z Position (Machine Coordinates) to begin the initial(seek) probe.", k.DISTANCE_SETTING),
+  createDefinition("SeekMaxDistance", "Seek Max Distance", "Maximum distance of travel from Z Seek Start on initial probe. Used to calculate a probe target and guard against gross over travel.", k.UDISTANCE_SETTING),
+  createDefinition("SeekFeedRate", "Seek Feed Rate", "Feedrate for the initial(seek) probe.", k.FEED_SETTING),
+  createDefinition("SeekRetreat", "Seek Retreat", "Distance to retreat after trigger, before a subsequent(set) probe.", k.UDISTANCE_SETTING),
+  createDefinition("SetFeedRate", "Set Feed Rate", "Feedrate for any subsequent(seek) probe.", k.FEED_SETTING),
 
+  --Tool Recognition
+  createDefinition("ToolRecognitionEnabled", "Tool Recognition Enabled", "Enable infrared tool recognition.", k.SWITCH_SETTING),
+  createDefinition("ToolRecognitionOverride", "Tool Recognition Override", "", k.SWITCH_SETTING),
+  createDefinition("IRPort", "IR Sensor Port", "Port number for the tool recognition IR sensor input.", k.PORT_SETTING),
+  createDefinition("IRPin", "IR Sensor Pin", "Port number for the tool recognition IR sensor input.", k.PIN_SETTING),
+  createDefinition("IRActiveBroken", "IR Active Broken", "Indication of whether the IR input is active when the beam is broken or when the beam is clear.", k.PIN_SETTING),
+  createDefinition("ZZone1", "Z Zone 1", "Z Position (Machine Coordinates) for confirming the absence of a nut when unloading and the presence of a nut when loading.", k.DISTANCE_SETTING),
+  createDefinition("ZZone2", "Z Zone 2", "Z Position (Machine Coordinates) for confirming complete threading when loading.", k.DISTANCE_SETTING),
 
---Define and create settings
-createSettingOption("Units", "Units", "Units for this configuration.", UNIT_OPTIONS)
-createSettingOption("Alignment", "Alignment", "Axis on which the magazine is aligned.", ALIGNMENT_OPTIONS)
-createSettingOption("Direction", "Direction", "Direction of travel from Pocket 1 to Pocket 2.", DIRECTION_OPTIONS)
-createSettingDouble("ZMoveToLoad", "Z Move To Load", "Z Position to rise to after unloading a tool, before moving to the pocket for loading.")
-createSettingDouble("ZMoveToProbe", "Z Move To Probe", "Z Position to rise to after loading a tool, before moving to the tool setter for probing.")
-createSettingDouble("ZSafeClearance", "Z Safe Clearance", "Z Position for the safe clearance of all obstacles.")
+  --Dust Cover
+  createDefinition("DustCoverEnabled", "Dust Cover Enabled", "Enable programmatic dust cover control.", k.SWITCH_SETTING),
+  createOptionDefinition("DustCoverControl", "Dust Cover Control", "Method for controlling the dust cover programmatically.", k.COVER_CONTROL_OPTIONS),
+  createOptionDefinition("DustCoverAxis", "Dust Cover Axis", "Designated axis for controlling the dust cover.", k.COVER_AXIS_OPTIONS),
+  createDefinition("DustCoverOpenPos", "Dust Cover Open Position", "Designated axis position (machine coordinates) at which the dust cover is fully open.", k.DISTANCE_SETTING),
+  createDefinition("DustCoverClosedPos", "Dust Cover Closed Position", "Designated axis position (machine coordinates) at which the dust cover is fully closed.", k.DISTANCE_SETTING),
+  createDefinition("DustCoverPort", "Dust Cover Port", "Port number for the dust cover output.", k.PORT_SETTING),
+  createDefinition("DustCoverPin", "Dust Cover Pin", "Pin number for the dust cover output.", k.PIN_SETTING),
+  createDefinition("DustCoverDwell", "Dust Cover Dwell", "Dwell time (seconds) to allow an output controlled dust cover to fully open or close.", k.DWELL_SETTING),
+  createDefinition("DustCoverOpenMCode", "Dust Cover Open M-Code", "The assigned M-Code for opening the dust cover.", k.MCODE_SETTING),
+  createDefinition("DustCoverCloseMCode", "Dust Cover Close M-Code", "The assigned M-Code for closing the dust cover.", k.MCODE_SETTING),
 
---Public function for updating a setting's value
-function RapidChangeSettings.SetValue(key, value)
-  local rc
+  --Hooks
+  createDefinition("BeforeChangeMCode", "Before Change M-Code", "The M-Code for a custom macro to run before every tool change.", k.MCODE_SETTING),
+  createDefinition("AfterChangeMCode", "After Change M-Code", "The M-Code for a custom macro to run after every tool change.", k.MCODE_SETTING),
+}
 
-  --Update storage
-  if RapidChangeSettings[key].storageType == DOUBLE_SETTING then
-    rc = mc.mcProfileWriteDouble(inst, RC_SECTION, key, value)
-  elseif RapidChangeSettings[key].storageType == INT_SETTING then
-    rc = mc.mcProfileWriteInt(inst, RC_SECTION, key, value)
+local function isChecked(value)
+  if value == k.ENABLED then
+    return true
+  elseif value == k.DISABLED then
+    return false
   else
-    rc = mc.mcProfileWriteString(inst, RC_SECTION, key, value)
+    return nil
+  end
+end
+
+local function getOptionLabels(options)
+  local labels = {}
+
+  for i, v in ipairs(options) do
+    labels[i] = v.label
   end
 
-  --Update setting in memory
-  RapidChangeSettings[key].value = getValue(key)
+  return labels
+end
 
-  return rc
+local function getSelectedIndex(options, value)
+  for i, v in ipairs(options) do
+    if v.value == value then
+      return i
+    else
+      return nil
+    end
+  end
+end
+
+--Setting Provider helper functions
+local function createUISetting(definition)
+  local value = RapidChangeSettings.GetValue(definition.key)
+
+  return {
+    key = definition.key,
+    label = definition.label,
+    description = definition.description,
+    settingType = definition.settingType,
+    optionLabels = getOptionLabels(definition.options),
+    selectedIndex = getSelectedIndex(definition.options, value),
+    isChecked = isChecked(value),
+    value = value,
+  }
+end
+
+--Setting Providers
+
+--Local lookup map
+local function buildDefinitionMap()
+  local map = {}
+
+  for _, v in ipairs(definitions) do
+    map[v.key] = v
+  end
+
+  return map
+end
+
+local definitionMap = buildDefinitionMap()
+
+--Retrieve a setting's value
+function RapidChangeSettings.GetValue(key)
+  local definition = definitionMap[key]
+
+  if definition.settingType == k.DISTANCE_SETTING or
+    definition.settingType == k.UDISTANCE_SETTING or
+    definition.settingType == k.FEED_SETTING or
+    definition.settingType == k.RPM_SETTING or
+    definition.settingType == k.DWELL_SETTING
+  then
+    return mc.mcProfileGetDouble(inst, RC_SECTION, definition.key, definition.defaultValue)
+  else
+    return mc.mcProfileGetInt(inst, RC_SECTION, definition.key, definition.defaultValue)
+  end
+end
+
+--Get an iterable list of settings for UI controls
+function RapidChangeSettings.GetUISettingsList()
+  local settings = {}
+
+  for i, v in ipairs(definitions) do
+    settings[i] = createUISetting(v)
+  end
+
+  return settings
+end
+
+--UIControl Registration
+--Register the UI control upon construction, unregister upon destruction.
+--This will allow for the handling of apapting control values to stored values to be handled by RapidChangeSettings.
+--The UI can fetch the settings list and create whichever appropriate controls it chooses and let the
+--settings worry about what to do with them. Indicate the control type used for the setting from the defined
+--control type constants when registering a control.
+local registeredControls = {}
+
+function RapidChangeSettings.RegisterUIControl(key, control, controlType)
+  registeredControls[key] = {
+    control = control,
+    controlType = controlType
+  }
+end
+
+function RapidChangeSettings.UnregisterUIControl(key)
+  --Is this enough for memory management?
+  registeredControls[key] = nil
+end
+
+--Call from UI to save user input
+--Settings will handle reading the input control and updating stored values
+function RapidChangeSettings.SaveUISettings()
+  --TODO: Save data from UI controls
 end
 
 return RapidChangeSettings
